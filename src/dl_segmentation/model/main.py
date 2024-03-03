de701acbe6b8ca9f5bce0f0f7c71861750affaac
Macpiedev <maciekpietrewicz@gmail.com>
2024-03-03 18:19:13 +0100

    Setting seed for torch and numpy

--- src/dl_segmentation/model/main.py
+++ src/dl_segmentation/model/main.py
@@ -1,7 +1,12 @@
 import random
+import torch
+import numpy as np
+
 from dl_segmentation.model.consts import MODEL_SEED
 
 random.seed(MODEL_SEED)
+torch.manual_seed(MODEL_SEED)
+np.random.seed(MODEL_SEED)
 
 from dl_segmentation.model.random_seed_test import print_random_ints
 
