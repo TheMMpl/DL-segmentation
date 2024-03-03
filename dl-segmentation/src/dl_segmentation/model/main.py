12b7b7b0769f62fbcdba986a79530d71ac3f02ae
Michał Kierul <michal@debian-BULLSEYE-live-builder-AMD64>
2024-03-03 15:52:48 +0100

    Random seed set.

---
+++ dl-segmentation/src/dl_segmentation/model/main.py
@@ -0,0 +1,9 @@
+import random
+from dl_segmentation.model.consts import MODEL_SEED
+
+random.seed(MODEL_SEED)
+
+from dl_segmentation.model.random_seed_test import print_random_ints
+
+print_random_ints(10)
+print(randint(1,100))
---
+++ dl-segmentation/src/dl_segmentation/model/random_seed_test.py
@@ -0,0 +1,5 @@
+import random
+
+def print_random_ints(n):
+	for i in range(n):
+		print(randint(1,100))
