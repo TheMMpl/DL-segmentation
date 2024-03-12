a54324439eb33af894b5dbb4cf2105cc9ac355ee
Mikołaj Hasik <mikoajhasik@gmail.com>
2024-03-12 22:29:34 +0100

    cleanup and example image

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -7,7 +7,7 @@ from dl_segmentation.model.model import model, weights
 
 model.eval()
 #tu testowy fragment danych
-img= read_image("istockphoto-1279831403-612x612.jpg")
+img= read_image("data/01_raw/istockphoto-1279831403-612x612.jpg")
 preprocess = weights.transforms()
 batch = preprocess(img).unsqueeze(0)
 prediction = model(batch)["out"]
