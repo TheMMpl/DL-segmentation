afd539cbbe6414edfb8121db56f38903814b373f
Mikołaj <mikoajhasik@gmail.com>
2024-04-28 16:47:25 +0200

    demo results

--- src/dl_segmentation/pipelines/reporting/nodes.py
+++ src/dl_segmentation/pipelines/reporting/nodes.py
@@ -3,10 +3,52 @@ import pandas as pd
 import plotly.express as px  # noqa:  F401
 import plotly.graph_objs as go
 import seaborn as sn
-
+import wandb
+from dl_segmentation.model.model import LightningModel
+from torchvision.datasets import Cityscapes
+from dl_segmentation.pipelines.train import CityScapesTransform
+from torchvision import transforms
+import torch
+from pathlib import Path
+from torch.utils.data import DataLoader
 
 # This function uses plotly.express
-def compare_passenger_capacity_exp(preprocessed_shuttles: pd.DataFrame):
+def check_model_inference(preprocessed_shuttles: pd.DataFrame):
+    # reference can be retrieved in artifacts panel
+    # "VERSION" can be a version (ex: "v2") or an alias ("latest or "best")
+    checkpoint_reference = "dlprojekt/DL_segmenation/model-jql8pobq:v20"
+    model_name="model-jql8pobq:v20"
+    # download checkpoint locally (if not already cached)
+    run = wandb.init(project="DL_segmenation")
+    artifact = run.use_artifact(checkpoint_reference, type="model")
+    artifact_dir = artifact.download()
+    print(artifact_dir)
+    # load checkpoint
+    model = LightningModel.load_from_checkpoint(Path(artifact_dir) / "model.ckpt",num_classes=34)
+    model.eval()
+    model.unet.eval()
+    val_dataset = Cityscapes('./dataset/cityscapes', split='val',target_type='semantic', transforms = CityScapesTransform())
+    #val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False,pin_memory=True)
+    #necessarry because we don't use the trainer here
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    trans = [transforms.v2.ToDtype(torch.float32, scale=True), transforms.v2.Resize((128,256))]
+    jank_iter=0
+    for img,target in val_dataset:
+        jank_iter+=1
+        # plt.imshow(torch.permute(img,(1,2,0)))
+        # plt.imshow(target[0])
+        # plt.show()
+        for t in trans:
+            img=t(img)
+        #print(img.shape)
+        image=img.to(device)
+        image.unsqueeze_(0)
+        #print(image.shape)
+        result=torch.argmax(model(image)[0],dim=0).cpu().detach().numpy()
+        # plt.imshow(result)
+        # plt.show()
+        plt.imsave(f'demo_results/res{jank_iter}.jpg',result)
+        
     return (
         preprocessed_shuttles.groupby(["shuttle_type"])
         .mean(numeric_only=True)
