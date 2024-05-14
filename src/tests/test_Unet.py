0a6f64427d217ce586cb4fdbf7324d74c33046e9
TheMMpl <mikoajhasik@gmail.com>
2024-05-14 05:00:57 +0200

    parametrize experiment

--- src/tests/test_Unet.py
+++ src/tests/test_Unet.py
@@ -3,12 +3,29 @@ import torch
 from torchvision.datasets import Cityscapes
 from torch.utils.data import DataLoader
 from torchvision.io.image import read_image
+from torchmetrics.classification import MulticlassJaccardIndex
 #from dl_segmentation.model import ResUnet
 from dl_segmentation.model.model import LightningModel
+import pytest
 
 
-def test_model():
-    unet=LightningModel(20)
-    print(unet.parameters())
+data_params=[]
+inference_params=[]
+@pytest.mark.parametrize("results_path,dataset_path",data_params)
+def prepare_data(results_path,dataset_path):
+    pass
+
+
+@pytest.mark.parametrize("model_path,results_path,dataset_path,test_size,metric",inference_params)
+def prepare_model():
+    pass
+ 
+
+@pytest.mark.parametrize("model_path,results_path,dataset_path,test_size,metric",inference_params)
+def test_model(model_path,results_path,dataset_path,test_size,metric):
+    
+
+def test_Unet(model_path,results_path,dataset_path,test_size,metric):
+
 #trainer = L.Trainer(limit_train_batches=2, max_epochs=1)
 #train_loader=DataLoader(Cityscapes)
