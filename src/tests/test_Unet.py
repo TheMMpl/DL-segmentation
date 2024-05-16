51a4f81658d6ab9a1d267013f590683f47827813
Mikołaj Hasik <mikoajhasik@gmail.com>
2024-05-16 15:44:37 +0200

    IoU test

--- src/tests/test_Unet.py
+++ src/tests/test_Unet.py
@@ -12,20 +12,20 @@ from kedro.io import DataCatalog
 import matplotlib.pyplot as plt
 from consts import NUM_CLASSES
 
-data_params=("demo_results/test_test",'./dataset/cityscapes')
+data_params=("demo_results/test_IoU",'./dataset/cityscapes')
 model_checkpoint='dlprojekt/DL_segmenation/model-daeah9nu:v37'
 test_size=10
 metric = MulticlassJaccardIndex(num_classes=NUM_CLASSES)
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def prepare_data(request):
     Path(request.param[0]).mkdir(parents=True, exist_ok=True)
     val_dataset = Cityscapes(request.param[1], split='val',target_type='semantic', transforms = CityScapesTransform())
     return val_dataset, request.param[0]
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def prepare_model(request):
     run = wandb.init(project="DL_segmenation")
     artifact = run.use_artifact(request.param, type="model")
@@ -37,12 +37,15 @@ def prepare_model(request):
     return model
  
 
-@pytest.mark.parametrize("test_size,metric,prepare_data,prepare_model",[(test_size,metric,data_params,model_checkpoint)],indirect=["prepare_data","prepare_model"])
+@pytest.mark.parametrize("test_size,metric,prepare_data,prepare_model",[(test_size,metric,data_params,model_checkpoint)],
+                         indirect=["prepare_data","prepare_model"])
 def test_model(test_size,metric,prepare_data,prepare_model):
+    """
+    test model inference on a number of images from the validation dataset and check the IoU metric relative to the ground truth
+    """
+    IoU=0
     model=prepare_model
     val_dataset,results_path=prepare_data
-    print(val_dataset)
-    print(test_size)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     trans = [transforms.v2.ToDtype(torch.float32, scale=True), transforms.v2.Resize((256,512))]
     image_num=0
@@ -52,9 +55,22 @@ def test_model(test_size,metric,prepare_data,prepare_model):
             img=t(img)
         image=img.to(device)
         image.unsqueeze_(0)
+        output=model(image)[0]
+        preds=output.view(output.size(0),-1).unsqueeze_(0)
+        target_tensor=target[0].flatten().unsqueeze_(0)
+        # print(output.shape)
+        # print(preds.shape[1])
+        # print(target_tensor.shape)
+        IoU+=metric(preds,target_tensor)
         result=torch.argmax(model(image)[0],dim=0).cpu().detach().numpy()
         comparison=np.vstack([result,target[0]])
         plt.imsave(f'{results_path}/res{image_num}.jpg',comparison)
         if image_num>test_size:
+            IoU=IoU/test_size  
+            #print(IoU)
+            assert IoU>=0.4
+            # f = open(f'{results_path}/IoU.txt', "a")
+            # f.write(str(IoU.item()))
+            # f.close()
             break
     
